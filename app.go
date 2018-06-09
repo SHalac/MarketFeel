32164a6660701e7ed3ad3c85a81f2153a94bbd29
Selim Halac <halacselim@gmail.com>
2018-06-09 10:54:55 -0600

    finished getting bearer from twitter app auth

--- app.go
+++ app.go
@@ -7,18 +7,37 @@ import (
 	"net/url"
 	"strings"
 	"encoding/base64"
+	"log"
+	"bytes"
+	"io/ioutil"
 )
 
 var (
 	consumerKey = secrets.API_KEY
 	consumerSecret = secrets.API_SECRET
+	bearerUrl = "https://api.twitter.com/oauth2/token"
 )
 
 
 
 func main() {
 	encodedToken := getBearerCred(consumerKey,consumerSecret)
+	body := []byte("grant_type=client_credentials")
+	req,err := http.NewRequest("POST",bearerUrl,bytes.NewBuffer(body))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Basic %s",encodedToken))
+	req.Header.Add("Content-Type","application/x-www-form-urlencoded;charset=UTF-8")
+	client := &http.Client{}
 
+	resp, err2 := client.Do(req)
+	if err2 != nil {
+		log.Fatal(err.Error())
+	}
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(respBody))
 }
 
 /* function to obtain bearer token credentials
