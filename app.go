355424a4d12f5dff4c727688f42231e0e6fcb870
Selim Halac <halacselim@gmail.com>
2018-06-11 00:44:28 -0600

    added bolt db funcs

--- app.go
+++ app.go
@@ -11,12 +11,15 @@ import (
 	"bytes"
 	"io/ioutil"
 	"encoding/json"
+	"errors"
+	"github.com/boltdb/bolt"
 )
 
 var (
 	consumerKey = secrets.API_KEY
 	consumerSecret = secrets.API_SECRET
 	bearerUrl = "https://api.twitter.com/oauth2/token"
+	bucketName = []byte("marketFeel")
 )
 
 
@@ -26,9 +29,67 @@ func main() {
 	if err != nil {
 		encodedToken := encodeToken(consumerKey,consumerSecret)
 		bearerToken := getBearer(encodedToken)
+		token = addTokenDb(bearerToken)
+		fmt.Println("added token to db")
 	}
+	fmt.Println(token)
 }
 
+/*
+Function: Get bearer token from database
+In: NONE
+out: token (string) and error, error is not nil 
+if token isn't found 
+*/
+func dbToken() (s string, err error){
+	db,err := bolt.Open("token.db",0600,&bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return "error", errors.New("Could not get token from db")
+	}
+	defer db.Close()
+	var token string
+	err = db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(bucketName)
+		if bucket == nil {
+			return errors.New("can't find main bucket")
+		}
+		token = bucket.Get([]byte("token"))
+		if token == nil {
+			return errors.New("can't find token key")
+		}
+		return nil
+		})
+	if err != nil {
+		return "error", errors.New("Could not get token from db")
+	}
+	return token
+
+}
+
+func addTokenDb(token string) error {
+	db,err := bolt.Open("token.db",0600,&bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return errors.New("Could not open db")
+	}
+	defer db.Close()
+	err2 = db.Update(func(tx *bolt.Tx) error {
+		bucket,err := tx.CreateBucketIfNotExists(bucketName)
+		if err != nil {
+			return err
+		}
+		err = bucket.Put("token",token)
+		})
+	if err2 != nil {
+		return errors.New("Could not add token to DB")
+	}
+	return nil
+}
+
+/*
+Function to get bearer token from twitter
+In: encoded token for request
+Out: access token string
+*/
 func getBearer(encodedToken) string{
 	body := []byte("grant_type=client_credentials")
 	req,err := http.NewRequest("POST",bearerUrl,bytes.NewBuffer(body))
@@ -54,9 +115,9 @@ func getBearer(encodedToken) string{
 	}
 }
 
-/* function to obtain bearer token credentials
+/* function to encode credentials to be used to get bearer
 input (2): consumer_key, consumer_secret
-output (1): token credential, base64 encoded
+output (1): encoded token, base64 encoded
 */
 func encodeToken(key string, secret string) string {
 	builder := strings.Builder{}
